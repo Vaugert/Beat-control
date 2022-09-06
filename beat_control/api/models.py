441993988242cca57866210397c00dd730e832d8
Vaugert <lindstromfabian@gmail.com>
2022-09-06 15:05:25 +0000

    serializers and first view made

--- beat_control/api/models.py
+++ beat_control/api/models.py
@@ -14,8 +14,7 @@ def generate_random_code():
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True)
+    code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
-    guest_can_paus = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
